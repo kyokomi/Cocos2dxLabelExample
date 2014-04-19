859a27a08d07e7ff862d16fb77ead0c05c1a2206
kyokomi <kyoko1220adword@gmail.com>
2014-04-19 15:21:36 +0900

    変数名がややこしいので修正。あとHelloWorldのLabelも削除

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -51,51 +51,44 @@ bool HelloWorld::init()
     /////////////////////////////
     // 3. add your codes below...
 
-    // add a label shows "Hello World"
-    // create and initialize a label
-    
-    auto label = LabelTTF::create("Hello World", "Arial", 24);
-    
-    // position the label on the center of the screen
-    label->setPosition(Point(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
-
-    // add the label as a child to this layer
-    this->addChild(label, 1);
-    
-
     const int fontSize = 80;
     
-    // デフォルトっぽいの
-    TTFConfig ttfConfig("fonts/Marker Felt.ttf", fontSize, GlyphCollection::DYNAMIC);
-    auto labelTest = Label::createWithTTF(ttfConfig, "hoge hoge");
-    labelTest->setPosition(Point(visibleSize.width/2 + origin.x, visibleSize.height * 0.8 + origin.y));
-    this->addChild(labelTest);
+    // デフォルトで入ってるMarker Felt.ttfフォントの設定を作る
+    TTFConfig ttfConfig("fonts/Marker Felt.ttf",
+                        fontSize,
+                        GlyphCollection::DYNAMIC);
+    // 1. 普通にTTFフォントを使って表示
+    auto label = Label::createWithTTF(ttfConfig, "hoge hoge");
+    label->setPosition(Point(visibleSize.width/2 + origin.x, visibleSize.height * 0.8 + origin.y));
+    this->addChild(label);
     
-    const int height = labelTest->getContentSize().height * 1.5;
+    const int height = label->getContentSize().height * 1.5;
     
-    // 文字と文字の間隔
+    // 2. 文字と文字の間隔を設定（アウトライン）
     TTFConfig ttfConfigOutLine(ttfConfig);
+    // 文字の間隔を指定（フォントサイズの幅っぽい）
     ttfConfigOutLine.outlineSize = 4;
-    auto labelTestOutLine = Label::createWithTTF(ttfConfigOutLine, "hoge hoge");
-    labelTestOutLine->setPosition(Point(labelTest->getPosition() - Point(0, height)));
-    this->addChild(labelTestOutLine);
+    auto labelOutLine = Label::createWithTTF(ttfConfigOutLine, "hoge hoge");
+    labelOutLine->setPosition(Point(label->getPosition() - Point(0, height)));
+    this->addChild(labelOutLine);
     
-    // 影
-    auto labelTestShadow = Label::createWithTTF(ttfConfig, "hoge hoge");
-    labelTestShadow->setPosition(Point(labelTestOutLine->getPosition() - Point(0, height)));
-    labelTestShadow->setColor(Color3B::RED);
-    labelTestShadow->enableShadow(Color4B(255,255,255,128), Size(4, -4));
-    this->addChild(labelTestShadow);
+    // 3. 文字に影をつける（x=4, y=-4の位置に影を置く）
+    auto labelShadow = Label::createWithTTF(ttfConfig, "hoge hoge");
+    labelShadow->setPosition(Point(labelOutLine->getPosition() - Point(0, height)));
+    labelShadow->setColor(Color3B::RED);
+    // 半透明の影を（x = 4, y = -4）の位置へ指定
+    labelShadow->enableShadow(Color4B(255,255,255,128), Size(4, -4));
+    this->addChild(labelShadow);
     
-    // エフェクト（outlineと併用できない）
+    // 4. エフェクト表示（アウトラインと併用できないので注意）
     TTFConfig ttfConfigEffect(ttfConfig);
+    // エフェクト有効（_useDistanceField = distanceFieldEnabled;されます）
     ttfConfigEffect.distanceFieldEnabled = true;
-    auto labelTestEffect = Label::createWithTTF(ttfConfigEffect, "hoge hoge");
-    labelTestEffect->setPosition(Point(labelTestShadow->getPosition() - Point(0, height)));
-    labelTestEffect->setColor(Color3B::WHITE);
-    labelTestEffect->enableGlow(Color4B::RED);
-    this->addChild(labelTestEffect);
+    auto labelEffect = Label::createWithTTF(ttfConfigEffect, "hoge hoge");
+    labelEffect->setPosition(Point(labelShadow->getPosition() - Point(0, height)));
+    // エフェクトの色を指定
+    labelEffect->enableGlow(Color4B::RED);
+    this->addChild(labelEffect);
     
     return true;
 }
